3e8c35d43cf502a0b0093297a70bbc2adc7fa973
taahasyedshams-24K0534 <taahasyed341@gmail.com>
2025-05-09 21:26:05 +0500

    Update task 10.cpp

--- lab 10/task 10.cpp
+++ lab 10/task 10.cpp
@@ -1,1 +1,20 @@
 
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ifstream file("data_records.txt");
+    if (!file) {
+        cout << "Failed to open data_records.txt" << endl;
+        return 1;
+    }
+
+    file.seekg(18, ios::beg); 
+    string record;
+    getline(file, record);
+    cout << record << endl;
+
+    file.close();
+}
