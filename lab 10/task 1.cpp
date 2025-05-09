655195415903880f54e40d6d10c55b162d21d250
taahasyedshams-24K0534 <taahasyed341@gmail.com>
2025-05-09 20:07:02 +0500

    Update task 1.cpp

--- lab 10/task 1.cpp
+++ lab 10/task 1.cpp
@@ -1,1 +1,24 @@
 
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main(){
+    ifstream file("vehicles.txt");
+    if(!file.is_open()){
+        cerr<<"Error: Could not open vehicles.txt"<<endl;
+        return 1;
+    }
+
+    string line;
+    while(getline(file,line)){
+        if(line.empty()||line.substr(0,2)=="//"){
+            continue;
+        }
+        cout<<"Read Line: "<<line<<endl;
+    }
+
+    file.close();
+    
+}
